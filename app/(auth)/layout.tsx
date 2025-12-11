28e1ea0c9a4e87733e28a9af92aa01fa02d7cf0d
Mario Bernard <95982886+mariobernard29@users.noreply.github.com>
2025-12-11 15:54:57 -0700

    Configuración inicial de Polar Stocks

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,19 +1,19 @@
 import Link from "next/link";
 import Image from "next/image";
-import {auth} from "@/lib/better-auth/auth";
-import {headers} from "next/headers";
-import {redirect} from "next/navigation";
+import { auth } from "@/lib/better-auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const Layout = async ({ children }: { children : React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
     const session = await auth.api.getSession({ headers: await headers() })
 
-    if(session?.user) redirect('/')
+    if (session?.user) redirect('/')
 
     return (
         <main className="auth-layout">
             <section className="auth-left-section scrollbar-hide-default">
                 <Link href="/" className="auth-logo">
-                    <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className='h-8 w-auto' />
+                    <Image src="/assets/icons/logo.svg" alt="polar stocks logo" width={140} height={32} className='h-8 w-auto' />
                 </Link>
 
                 <div className="pb-6 lg:pb-8 flex-1">{children}</div>
@@ -22,7 +22,7 @@ const Layout = async ({ children }: { children : React.ReactNode }) => {
             <section className="auth-right-section">
                 <div className="z-10 relative lg:mt-4 lg:mb-16">
                     <blockquote className="auth-blockquote">
-                        Signalist turned my watchlist into a winning list. The alerts are spot-on, and I feel more confident making moves in the market
+                        Polar Stocks turned my watchlist into a winning list. The alerts are spot-on, and I feel more confident making moves in the market
                     </blockquote>
                     <div className="flex items-center justify-between">
                         <div>
